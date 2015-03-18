ebc83a6917e980197cc4ef0e57a47e18e4ddbe30
Arturs Sefers <zanpakutos@gmail.com>
2015-03-18 21:41:59 +0200

    Update for UI.ProgressBar

--- src/plugins/UI/UI.ProgressBar.js
+++ src/plugins/UI/UI.ProgressBar.js
@@ -5,7 +5,24 @@ UI.ProgressBar = Entity.Geometry.extend
 	init: function(texture, fillTexture) 
 	{
 		var fill = new Entity.Geometry(fillTexture);
-		this.attach(fill);
+		this.attach(fill);		
+		this.updateUnits();
+	},
+
+	updateProgress: function()
+	{
+		console.log(this._fillWidth);
+		var units = this._fillWidth 
+	},
+
+	updateUnits: function()
+	{
+		var texture = this.children[0]._texture;
+
+		if(texture._loaded) {
+			this._fillWidth = texture.fullWidth
+		}
+
 	},
 
 	set min(value) 
@@ -13,7 +30,7 @@ UI.ProgressBar = Entity.Geometry.extend
 		if(this._min === value) { return; }
 
 		this._min = value;
-		this.updateElement();
+		this.updateProgress();
 	},
 
 	set max(value) 
@@ -21,7 +38,7 @@ UI.ProgressBar = Entity.Geometry.extend
 		if(this._max === value) { return; }
 
 		this._max = value;
-		this.updateElement();
+		this.updateProgress();
 	},
 
 	set value(value) 
@@ -29,24 +46,31 @@ UI.ProgressBar = Entity.Geometry.extend
 		if(this._value === value) { return; }
 
 		this._value = value;
-		this.updateElement();
+		this.updateProgress();
 	},
 
+	set percents(value) {
+		this.children[0].width = this._fillWidth / 100 * value;
+	},
 
 	get min() { return this._min; },
 	get max() { return this._max; },
 	get value() { return this._value; },
+	get percents() { return this._percents; },
 
 
 	set fillTexture(fillTexture) {
 		this.children[0].texture = fillTexture;
+		this.updateProgress();
 	},
 
-	get fillTexture() { return this.children[0].texture; },	
+	get fillTexture() { return this.children[0]._texture; },	
 
 
 	//
 	_min: 0,
 	_max: 100,
-	_value: 0
+	_value: 0,
+	_unit: 1,
+	_fillWidth: 1
 });
