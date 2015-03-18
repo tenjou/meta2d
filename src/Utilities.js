a6f9e4a1efd935af3d7a834c66db263b2eb93ca5
Arturs Sefers <zanpakutos@gmail.com>
2015-03-18 21:40:34 +0200

    Added tokenises utilities.

--- src/Utilities.js
+++ src/Utilities.js
@@ -78,7 +78,7 @@ meta.loadSpriteSheet = function(buffer, folderPath, tag)
  * @param buffer {Array|String} Buffer with sound sources.
  * @param folderPath {String=} Path applied to sound sources.
  */
-meta.loadFont = function(buffer, folderPath, tag)
+meta.preloadFont = function(buffer, folderPath, tag)
 {
 	if(!meta._preloadResource("Font", buffer, folderPath, tag)) {
 		console.warn("(meta.loadFont) Unsupported parameter was passed.");
@@ -393,4 +393,39 @@ meta.toHex = function(c) {
 
 meta.rgbToHex = function(r, g, b) {
     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-}
+};
+
+function isSpace(c) {
+	return (c === " " || c === "\t" || c === "\r" || c === "\n");
+};
+
+function isNewline(c) {
+	return (c === "\r" || c === "\n");
+};
+
+function isDigit(c) {
+	return (c >= "0" && c <= "9") || (c === ".");
+};
+
+function isAlpha(c) 
+{
+	return (c >= "a" && c <= "z") ||
+		   (c >= "A" && c <= "Z") ||
+		   (c == "_" && c <= "$");
+};
+
+function isAlphaNum(c) 
+{
+	return (c >= "a" && c <= "z") ||
+		   (c >= "A" && c <= "Z") ||
+		   (c >= "0" && c <= "9") ||
+		   c === "_" || c === "$";
+};
+
+function isBinOp(c) 
+{
+	return (c === "=" || c === "!" || c === "<" || c === ">" || 
+			c === "+" || c === "-" || c === "*" || c === "/" ||
+			c === "&" || c === "~" || c === "|" || c === "%");
+};
+
