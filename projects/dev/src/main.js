8359cb6c4ebe2f9e845154a5d51c358a52d3c17c
Tenjou <zanpakutos@gmail.com>
2015-07-30 21:09:52 +0300

    Update to particle entity.

--- projects/dev/src/main.js
+++ projects/dev/src/main.js
@@ -4,7 +4,8 @@ var stats = null;
 
 meta.load = function()
 {
-	meta.renderer.bgColor = "#777";
+	meta.renderer.bgColor = "#000";
+	meta.engine.resize(250, 300)
 
 	meta.loadTexture("assets/particle");
 
@@ -31,22 +32,62 @@ meta.load = function()
 	var cfg = emitter.cfg;
 
 	var gui = new dat.GUI();
-	gui.add(emitter, "togglePlay");
-	gui.add(emitter, "reset");
+	gui.add(emitter, "togglePlay").name("Play");
+	gui.add(emitter, "reset").name("Reset");
+
+	//var presets = gui.addFolder("Presets");
+	// /presets.add(emitter, "presets");
+
 	gui.add(cfg, "numMax").min(0).max(1000);
-	gui.add(cfg, "emissionRate").min(0).max(1000);
-	gui.add(cfg, "life").min(0).max(1000).step(0.1);
-	gui.add(cfg, "lifeVar").min(0).max(1000).step(0.1);
-	var guiPos = gui.addFolder("pos");
-	guiPos.add(emitter, "x").min(0).max(2000);
-	guiPos.add(emitter, "y").min(0).max(2000);
-	guiPos.add(cfg, "xVar").min(0).max(2000);
-	guiPos.add(cfg, "yVar").min(0).max(2000);
-	var guiPhysics = gui.addFolder("physics");	
-	guiPhysics.add(cfg, "speed").min(-500).max(500);
-	guiPhysics.add(cfg, "speedVar").min(-1000).max(1000).step(0.1);
-	guiPhysics.add(cfg, "angle").min(0).max(360);
-	guiPhysics.add(cfg, "angleVar").min(0).max(360);	
+	// gui.add(cfg, "emissionRate").min(0).max(1000);
+	// gui.add(cfg, "life").min(0).max(1000).step(0.1);
+	// gui.add(cfg, "lifeVar").min(0).max(1000).step(0.1);
+	// var guiPos = gui.addFolder("Position");
+	// guiPos.add(emitter, "x").min(0).max(2000);
+	// guiPos.add(emitter, "y").min(0).max(2000);
+	// guiPos.add(cfg, "xVar").min(0).max(2000);
+	// guiPos.add(cfg, "yVar").min(0).max(2000);
+
+	// var color = gui.addFolder("Visual");
+	// var startColor = color.addFolder("startColor");
+	// startColor.add(cfg.startColor, "0").min(0).max(255).name("red");
+	// startColor.add(cfg.startColor, "1").min(0).max(255).name("green");
+	// startColor.add(cfg.startColor, "2").min(0).max(255).name("blue");
+	// startColor.add(cfg.startColor, "3").min(0).max(1).step(0.01).name("alpha");
+	// var startColorVar = color.addFolder("startColorVar");
+	// startColorVar.add(cfg.startColorVar, "0").min(0).max(255).name("red");
+	// startColorVar.add(cfg.startColorVar, "1").min(0).max(255).name("green");
+	// startColorVar.add(cfg.startColorVar, "2").min(0).max(255).name("blue");
+	// startColorVar.add(cfg.startColorVar, "3").min(0).max(1).step(0.01).name("alpha");
+	// var endColor = color.addFolder("endColor");
+	// endColor.add(cfg.endColor, "0").min(0).max(255).name("red");
+	// endColor.add(cfg.endColor, "1").min(0).max(255).name("green");
+	// endColor.add(cfg.endColor, "2").min(0).max(255).name("blue");
+	// endColor.add(cfg.endColor, "3").min(0).max(1).step(0.01).name("alpha");
+	// var endColorVar = color.addFolder("endColorVar");
+	// endColorVar.add(cfg.endColorVar, "0").min(0).max(255).name("red");
+	// endColorVar.add(cfg.endColorVar, "1").min(0).max(255).name("green");
+	// endColorVar.add(cfg.endColorVar, "2").min(0).max(255).name("blue");
+	// endColorVar.add(cfg.endColorVar, "3").min(0).max(1).step(0.01).name("alpha");
+	// color.add(cfg, "scale").min(0).max(100);
+	// color.add(cfg, "scaleVar").min(0).max(100);
+	// color.add(cfg, "endScale").min(0).max(100);
+	// color.add(cfg, "endScaleVar").min(0).max(100);
+
+	//var textureFolder = gui.addFolder("Texture");
+	//textureFolder.add(cfg, "textured")
+
+	// var guiPhysics = gui.addFolder("Physics");	
+	// guiPhysics.add(cfg, "speed").min(-500).max(500);
+	// guiPhysics.add(cfg, "speedVar").min(-1000).max(1000).step(0.1);
+	// guiPhysics.add(cfg, "angle").min(0).max(360);
+	// guiPhysics.add(cfg, "angleVar").min(0).max(360);
+	// guiPhysics.add(cfg, "gravityX").min(-500).max(500);
+	// guiPhysics.add(cfg, "gravityY").min(-500).max(500);
+	// guiPhysics.add(cfg, "radialAccel").min(-500).max(500);
+	// guiPhysics.add(cfg, "radialAccelVar").min(-500).max(500);
+	// guiPhysics.add(cfg, "tangentialAccel").min(-500).max(500);
+	// guiPhysics.add(cfg, "tangentialAccelVar").min(-500).max(500);
 }
 
 meta.render = function()
--- src/entity/Entity.Particle.js
+++ src/entity/Entity.Particle.js
@@ -10,6 +10,9 @@ meta.class("Entity.Particle", "Entity.Geometry",
 		for(var n = 0; n < max; n++) {
 			this.particles[n] = new this.Particle();
 		}
+
+		this._canvas = document.createElement("canvas");
+		this._ctx = this._canvas.getContext("2d");
 	},
 
 	update: function(tDelta) 
@@ -71,35 +74,85 @@ meta.class("Entity.Particle", "Entity.Geometry",
 		var angle = this.cfg.angle + (meta.random.numberF(-1, 1) * this.cfg.angleVar);
 		particle.velX = Math.cos((Math.PI * angle) / 180) * speed;
 		particle.vecY = -Math.sin((Math.PI * angle) / 180) * speed;
-		particle.velX = 0;
-		particle.velY = 0;
+
+		particle.radialAccel = this.cfg.radialAccel + (this.cfg.radialAccelVar * meta.random.numberF(-1, 1));
+		particle.tangentialAccel = this.cfg.tangentialAccel + (this.cfg.tangentialAccelVar * meta.random.numberF(-1, 1));
 
 		particle.color[0] = this.cfg.startColor[0] + (this.cfg.startColorVar[0] * meta.random.numberF(-1, 1));
 		particle.color[1] = this.cfg.startColor[1] + (this.cfg.startColorVar[1] * meta.random.numberF(-1, 1));
 		particle.color[2] = this.cfg.startColor[2] + (this.cfg.startColorVar[2] * meta.random.numberF(-1, 1));
 		particle.color[3] = this.cfg.startColor[3] + (this.cfg.startColorVar[3] * meta.random.numberF(-1, 1));
+
+		this._endColor[0] = this.cfg.endColor[0] + (this.cfg.endColorVar[0] * meta.random.numberF(-1, 1));
+		this._endColor[1] = this.cfg.endColor[1] + (this.cfg.endColorVar[1] * meta.random.numberF(-1, 1));
+		this._endColor[2] = this.cfg.endColor[2] + (this.cfg.endColorVar[2] * meta.random.numberF(-1, 1));
+		this._endColor[3] = this.cfg.endColor[3] + (this.cfg.endColorVar[3] * meta.random.numberF(-1, 1));
+
+		particle.colorDelta[0] = (this._endColor[0] - this.cfg.startColor[0]) / particle.life;
+		particle.colorDelta[1] = (this._endColor[1] - this.cfg.startColor[1]) / particle.life;
+		particle.colorDelta[2] = (this._endColor[2] - this.cfg.startColor[2]) / particle.life;
+		particle.colorDelta[3] = (this._endColor[3] - this.cfg.startColor[3]) / particle.life;
+
+		particle.scale = this.cfg.scale + (this.cfg.scaleVar * meta.random.numberF(-1, 1));
+		var endScale = this.cfg.endScale + (this.cfg.endScaleVar * meta.random.numberF(-1, 1));
+		particle.scaleDelta = (endScale - particle.scale) / particle.life;
 	},
 
 	updateParticle: function(particle, tDelta)
 	{
+		particle.forcesX = (this.cfg.gravityX) * tDelta;
+		particle.forcesY = (this.cfg.gravityY) * tDelta;
+		particle.velX += particle.forcesX;
+		particle.vecY += particle.forcesY
+
 		particle.x += particle.velX * tDelta;
 		particle.y += particle.vecY * tDelta;
+
+		if(particle.color)
+		{
+			particle.color[0] += particle.colorDelta[0] * tDelta;
+			particle.color[1] += particle.colorDelta[1] * tDelta;
+			particle.color[2] += particle.colorDelta[2] * tDelta;
+			particle.color[3] += particle.colorDelta[3] * tDelta;
+		}
+
+		particle.scale += this.scaleDelta;
 	},
 
 	draw: function(ctx)
 	{
 		var tDelta = meta.time.deltaF;
-
-		var particle;
 		var img = this.texture.canvas;
 		var parentX = this.volume.minX;
 		var parentY = this.volume.minY;
 
-		for(var n = 0; n < this.numActive; n++) {
+		var particle, color;
+		for(var n = 0; n < this.numActive; n++) 
+		{
 			particle = this.particles[n];
-			// particle.x += (particle.velocityX * tDelta);
-			// particle.y += (particle.velocityY * tDelta);
-			ctx.drawImage(img, parentX + particle.x, parentY + particle.y);
+			color = particle.color;
+
+			this._ctx.clearRect(0, 0, this._canvas.width, this._canvas.height);
+			this._ctx.globalCompositeOperation = "source-over";
+			this._ctx.globalAlpha = color[3];
+			this._ctx.drawImage(img, 0, 0);
+
+			this._ctx.globalCompositeOperation = "source-atop";
+			this._ctx.fillStyle = "rgba(" + 
+				(color[0] | 0) + ", " + 
+				(color[1] | 0) + ", " + 
+				(color[2] | 0) + ", 1.0)";
+// console.log("rgba(" + 
+// 				(color[0] | 0) + ", " + 
+// 				(color[1] | 0) + ", " + 
+// 				(color[2] | 0) + ", 1.0)");
+			this._ctx.fillRect(0, 0, this._canvas.width, this._canvas.height);
+
+			this._ctx.globalCompositeOperation = "source-over";
+			this._ctx.globalAlpha = color[3];			
+
+			ctx.globalCompositeOperation = 'lighter';
+			ctx.drawImage(this._canvas, parentX + particle.x, parentY + particle.y);
 		}
 	},
 
@@ -126,16 +179,45 @@ meta.class("Entity.Particle", "Entity.Geometry",
 
 	},
 
+	setTexture: function(texture)
+	{
+		this._super(texture);
+
+		if(!this.texture) 
+		{
+			if(this._cache.texture) {
+				this._svgTexture = new Resource.SVG();
+				this._svgTexture.fillStyle = "white";
+				this._svgTexture.circle(this.cfg.radius);
+			}
+			this.texture = this._svgTexture;
+		}
+	},
+
+	updateFromTexture: function() 
+	{
+		this._super();
+
+		this._canvas.width = this.texture.width;
+		this._canvas.height = this.texture.height;
+	},
+
 	Particle: function()
 	{
+		this.life = 0;
 		this.x = 0;
 		this.y = 0;
 		this.velX = 0;
 		this.velY = 0;
-		this.life = 0;
+		this.radialAccel = 0;
+		this.tangentialAccel = 0;
+		this.forcesX = 0;
+		this.forcesY = 0;
 
 		this.color = new Float32Array(4);
 		this.colorDelta = new Float32Array(4);
+		this.scale = 1;
+		this.scaleDelta = 1;
 	},
 
 	cfg: 
@@ -152,12 +234,25 @@ meta.class("Entity.Particle", "Entity.Geometry",
 		speed: 15,
 		speedVar: 5,
 		angle: 90,
-		angleVar: 139,
+		angleVar: 360,
+		gravityX: -200,
+		gravityY: -200,
+		radialAccel: 0,
+		radialAccelVar: 0,
+		tangentialAccel: 0,
+		tangentialAccelVar: 0,
 
-		startColor: new Float32Array(4),
-		startColorVar: new Float32Array(4),
-		endColor: new Float32Array(4),
-		endColorVar: new Float32Array(4)		
+		startColor: [ 51, 102, 179, 1 ],
+		startColorVar: [ 0, 0, 51, 0.1 ],
+		endColor: [ 0, 0, 0, 1 ],
+		endColorVar: [ 0, 0, 0, 0 ],
+		scale: 1.0,
+		scaleVar: 1.0,
+		endScale: 1.0,
+		endScaleVar: 1.0,
+
+		texture: null,
+		radius: 10
 	},
 
 	//
@@ -169,5 +264,10 @@ meta.class("Entity.Particle", "Entity.Geometry",
 	elapsed: 0,
 	duration: Infinity,
 
-	addictive: false
+	addictive: false,
+
+	_endColor: new Float32Array(4),
+	_canvas: null,
+	_ctx: null,
+	_svgTexture: null
 });
