5cfa9a1ddd7953e8903b1799df6176b4e2df6d52
Tenjou <zanpakutos@gmail.com>
2015-08-10 09:30:48 +0300

    Added new loading options for tilemap.

--- src/entity/Entity.Tilemap.js
+++ src/entity/Entity.Tilemap.js
@@ -31,8 +31,6 @@ meta.class("Entity.TilemapLayer", "Entity.Geometry",
 				var gidX = (gid % 8) * this.tileWidth;
 				var gidY = ((gid / 8) | 0) * this.tileHeight;
 
-				//console.log(gidX, gidY)
-
 				ctx.drawImage(canvas, gidX, gidY, this.tileWidth, this.tileHeight, posX, posY, this.tileWidth, this.tileHeight);
 				posX += this.tileWidth;
 			}
@@ -88,14 +86,76 @@ meta.class("Entity.Tilemap", "Entity.Geometry",
 		});
 	},
 
-	createLayer: function(name, tilesX, tiles)
+	create: function(tilesX, tilesY, tileWidth, tileHeight)
 	{
+		this.tilesX = tilesX;
+		this.tilesY = tilesY;
+		this.tileWidth = tileWidth;
+		this.tileHeight = tileHeight;
 
+		this.resize(tilesX * tileWidth, tilesY * tileHeight);
+
+		this.tilesets = [];
+		this.detachAll();
+	},
+
+	createTileset: function(gid, tileWidth, tileHeight, imgSrc)
+	{
+		var wildcardIndex = imgSrc.lastIndexOf(".");
+		var slashIndex = imgSrc.lastIndexOf("/");
+		if(slashIndex === -1) {
+			slashIndex = 0;
+		}
+		var imgName = imgSrc.substr(slashIndex, wildcardIndex, wildcardIndex - slashIndex);
+
+		var texture = meta.resources.getTexture(imgName);
+		if(!texture) {
+			texture = new Resource.Texture(this.folderPath + imgSrc);
+		}
+
+		var tileset = new this.Tileset(gid, tileWidth, tileHeight, texture);
+		this.tilesets.push(tileset);
+	},
+
+	createLayer: function(name, tilesX, tilesY, data)
+	{
+		var layer = new Entity.TilemapLayer();
+		layer.name = name;
+		layer.tilesX = tilesX;
+		layer.tilesY = tilesY;
+		layer.tileWidth = this.tileWidth;
+		layer.tileHeight = this.tileHeight;
+		layer.resize(tilesX * this.tileWidth, tilesY * this.tileHeight);
+		layer.data = data;
+		layer.tileset = this.tilesets[0];
+		this.attach(layer);
+
+		return layer;
 	},
 
 	_parse_json: function(data)
 	{
-		console.log("json");
+		var json = JSON.parse(data);
+
+		this.create(json.width, json.height, json.tilewidth, json.tileheight);
+
+		var tileset;
+		var tilesets = json.tilesets;
+		var num = tilesets.length;
+		for(var n = 0; n < num; n++) 
+		{
+			tileset = tilesets[n];
+			this.createTileset(tileset.firstgid, tileset.tileWidth, tileset.tileHeight, tileset.image);
+		}
+
+		var layer;
+		var layers = json.layers;
+		num = layers.length;
+		for(n = 0; n < num; n++)
+		{
+			layer = layers[n];
+			this.createLayer(layer.name, layer.width, layer.height, layer.data);
+		}
 
 		this.loaded = true;
 	},
@@ -104,14 +164,13 @@ meta.class("Entity.Tilemap", "Entity.Geometry",
 	{
 		var parser = new DOMParser();
 		var xml = parser.parseFromString(data, "text/xml");
-
 		var node = xml.documentElement;
-		this.tilesX = parseInt(node.getAttribute("width"));
-		this.tilesY = parseInt(node.getAttribute("height"));
-		this.tileWidth = parseInt(node.getAttribute("tilewidth"));
-		this.tileHeight = parseInt(node.getAttribute("tilewidth"));
 
-		this.resize(this.tilesX * this.tileWidth, this.tilesY * this.tileHeight);
+		this.create(
+			parseInt(node.getAttribute("width")),
+			parseInt(node.getAttribute("height")),
+			parseInt(node.getAttribute("tilewidth")),
+			parseInt(node.getAttribute("tilewidth")));
 
 		var childNodes = node.childNodes;
 		var numNodes = childNodes.length;
@@ -120,8 +179,13 @@ meta.class("Entity.Tilemap", "Entity.Geometry",
 			node = childNodes[i];
 			if(node.nodeType !== 1) { continue; }
 
-			if(node.nodeName === "tileset") {
-				this._parse_tmx_tileset(node);
+			if(node.nodeName === "tileset") 
+			{
+				this.createTileset(
+					parseInt(node.getAttribute("firstgid")),
+					parseInt(node.getAttribute("tileWidth")),
+					parseInt(node.getAttribute("tileHeight")),
+					node.childNodes[1].getAttribute("source"));
 			}
 			else if(node.nodeName === "layer") {
 				this._parse_tmx_layer(node);
@@ -135,53 +199,33 @@ meta.class("Entity.Tilemap", "Entity.Geometry",
 		this.loaded = true;
 	},
 
-	_parse_tmx_tileset: function(node)
-	{
-		var imgNode = node.childNodes[1];
-		var imgSrc = imgNode.getAttribute("source");
-		var wildcardIndex = imgSrc.lastIndexOf(".");
-		var slashIndex = imgSrc.lastIndexOf("/");
-		if(slashIndex === -1) {
-			slashIndex = 0;
-		}
-		var imgName = imgSrc.substr(slashIndex, wildcardIndex, wildcardIndex - slashIndex);
-
-		var texture = meta.resources.getTexture(imgName);
-		if(!texture) {
-			texture = new Resource.Texture(this.folderPath + imgSrc);
-		}
-
-		var tileset = new this.Tileset();
-		tileset.gid = parseInt(node.getAttribute("firstgid"));
-		tileset.name = node.getAttribute("name");
-		tileset.tileWidth = parseInt(node.getAttribute("tileWidth"));
-		tileset.tileHeight = parseInt(node.getAttribute("tileHeight"));
-		tileset.texture = texture;
-
-		this.tilesets.push(tileset);
-	},
-
 	_parse_tmx_layer: function(node)
 	{
-		var layer = new Entity.TilemapLayer();
-		layer.name = node.getAttribute("name");
-		layer.tilesX = parseInt(node.getAttribute("width"));
-		layer.tilesY = parseInt(node.getAttribute("height"));
-		layer.tileWidth = this.tileWidth;
-		layer.tileHeight = this.tileHeight;
-		layer.resize(layer.tilesX * this.tileWidth, layer.tilesY * this.tileHeight);
+		var name = node.getAttribute("name");
+		var tilesX = parseInt(node.getAttribute("width"));
+		var tilesY = parseInt(node.getAttribute("height"));
+
+		var dataNode = node.children[0];
+		var encoding = dataNode.getAttribute("encoding");
 
 		var n;
-		var num = layer.tilesX * layer.tilesY;
+		var num = tilesX * tilesY;
 		var data = new Uint32Array(num);
-		var dataNode = node.children[0];
 
-		var encoding = dataNode.getAttribute("encoding");
-		if(encoding === "csv")
+		if(encoding)
 		{
-			var strData = dataNode.innerHTML.split(",");
-			if(strData.length !== num) {
-				console.warn("(Entity.Tilemap.parse) Layer resolution does not match with data size");
+			var strData = null;
+
+			if(encoding === "csv")
+			{
+				strData = dataNode.innerHTML.split(",");
+				if(strData.length !== num) {
+					console.warn("(Entity.Tilemap._parse_tmx) Layer resolution does not match with data size");
+					return;
+				}
+			}
+			else {
+				console.warn("(Entity.Tilemap._parse_tmx) Unsupported layer encoding used: " + encoding);
 				return;
 			}
 
@@ -199,20 +243,18 @@ meta.class("Entity.Tilemap", "Entity.Geometry",
 				node = dataNodes[n];
 				if(node.nodeType !== 1) { continue; }
 
-				data[id++] = node.getAttribute("gid");
+				data[id++] = parseInt(node.getAttribute("gid"));
 			}
 		}
 
-		layer.data = data;
-		layer.tileset = this.tilesets[0];
-		this.attach(layer);
+		this.createLayer(name, tilesX, tilesY, data);
 	},
 
-	Tileset: function() {
-		this.gid = 0;
-		this.texture = null;
-		this.tileWidth = 0;
-		this.tileHeight = 0;
+	Tileset: function(gid, tileWidth, tileHeight, texture) {
+		this.gid = gid;
+		this.tileWidth = tileWidth;
+		this.tileHeight = tileHeight;
+		this.texture = texture;
 	},
 
 	//
