0997811a1406caaf5fe3d6bedfec42d504de3df3
Arturs Šefers <arthur.der.shefer@gmail.com>
2016-07-13 22:55:46 +0300

    Added meta.getNameFromPath.

--- new/src/utils.js
+++ new/src/utils.js
@@ -3,4 +3,17 @@
 meta.isPowerOfTwo = function(x)
 {
 	return ((x != 0) && ((x & (~x + 1)) == x));
-}
+};
+
+meta.getNameFromPath = function(path)
+{
+	var wildcardIndex = path.lastIndexOf(".");
+	var slashIndex = path.lastIndexOf("/");
+
+	// If path does not have a wildcard:
+	if(wildcardIndex < 0 || (path.length - wildcardIndex) > 5) { 
+		return path.slice(slashIndex + 1);
+	}
+
+	return path.slice(slashIndex + 1, wildcardIndex);
+};
