8db6659ca071b023ee23c45988443fdf4479f2ae
Tenjou <arthur.der.shefer@gmail.com>
2019-09-08 14:56:42 +0300

    add: debug panel

---
+++ src/Debug.js
@@ -0,0 +1,40 @@
+import Renderable from "./entity/Renderable"
+import Text from "./entity/Text"
+import Engine from "./Engine"
+import Time from "./Time"
+
+const debugLayer = 7
+let debugPanel = null
+let fpsText = null
+let active = false
+
+const load = () => {
+	debugPanel = new Renderable()
+	Engine.view.addChild(debugPanel)
+
+	fpsText = new Text()
+	fpsText.position.set(5, 5)
+	debugPanel.addChild(fpsText)
+	
+	debugPanel.setLayer(7)
+}
+
+const update = () => {
+	if(!active) {
+		return
+	}
+	fpsText.text = `fps ${Time.fps} | ${Time.ms}ms`
+}
+
+const toggle = () => {
+	active = !active
+	if(active && !debugPanel) {
+		load()
+		update()
+	}
+	Engine.camera.setCullMask(debugLayer, active)
+}
+
+export {
+	update, toggle
+}
